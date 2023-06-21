b4d7604304ffec0236833f43fb2ddf57c2d9a911
David-CapData <133758845+David-CapData@users.noreply.github.com>
2023-06-21 11:35:39 +0200

    [Main] Refonte des commentaires

--- Scripts/get_jira_custom_choices_allowed_values.py
+++ Scripts/get_jira_custom_choices_allowed_values.py
@@ -1,30 +1,41 @@
+# Importation des librairies utilisées
+# Import of the used libraries
 import requests
 import json
 
-# Récupération des credentials
+# Récupération des identifiants
+# Recuperation of the IDs
 creds_jira = ("ID_for_JIRA","Token-API")
 
-# Ligne pour call l'api du ticket avec les creds en option
+# Appel de L'API via "requests" avec les identifiants
+# Call of the api via "requests" with the IDs
 r = requests.get("https://testdavidcapdata.atlassian.net/rest/api/3/issue/T1-4/editmeta", auth=creds_jira)
 
-# Boucle If pour vérifier la possibilité de "GET" l'api
+# IF pour vérifier si l'appel de l'api fonctionne
+# IF to verify if the call of the API works
 if r.status_code == 200:
-    # Si ça fontionne, print 200
+    # Affichage du statut de l'appel de l'api
+    # Display of API call status 
     print("the status of the get command is : " + str(r.status_code))
 
-    # Les données de l'api dans la variable data
+    # Création d'une variable contenant toue les données de l'API
+    # Creation of variable containing all the data of the API
     data = r.json()
 
     # Formatage du Json
+    # Formatting of Json
     Human_readable_data = json.dumps(data, indent=4)
 
     # Récupération des valeurs possible d'un custom field
+    # Fetch of all possible values of a custom field
     Possible_value1 = r.json()['fields']['customfield_10036']['allowedValues'][0]['value'] # fields.customfield_10036.allowedValues[0].value
     Possible_value2 = r.json()['fields']['customfield_10036']['allowedValues'][1]['value'] # fields.customfield_10036.allowedValues[1].value
 
     # Affichage en Json formaté
+    # Display of the formatted Json
     print("Les valeurs possible du Customfiel2 sont: " + Possible_value1 + " & " + Possible_value2)
 
 else:
-    # Print en cas de non possibilité d'appel de L'api
+    # Affichage du statut de l'appel de l'api
+    # Display of API call status 
     print("the status of the get command is : " + str(r.status_code))
